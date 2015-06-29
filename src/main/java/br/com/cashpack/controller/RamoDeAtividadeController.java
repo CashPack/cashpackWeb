796f9bacaf3832ce094da1cb200a9a67b083fe2d
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2015-06-29 16:40:52 -0300

    Implementação da listagem de todos os tipos de atividades para uma empresa

---
+++ src/main/java/br/com/cashpack/controller/RamoDeAtividadeController.java
@@ -0,0 +1,32 @@
+package br.com.cashpack.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.roo.addon.web.mvc.controller.json.RooWebJson;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import br.com.cashpack.model.RamoDeAtividade;
+
+@RooWebJson(jsonObject = RamoDeAtividade.class)
+@Controller
+@RequestMapping("/ramoDeAtividade")
+public class RamoDeAtividadeController {
+
+	 @RequestMapping(headers = "Accept=application/json")
+	    @ResponseBody
+	    public ResponseEntity<String> listJson() {
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.add("Content-Type", "application/json; charset=utf-8");
+	        try {
+	            List<RamoDeAtividade> result = RamoDeAtividade.findAllRamoDeAtividades();
+	            return new ResponseEntity<String>(RamoDeAtividade.toJsonArray(result), headers, HttpStatus.OK);
+	        } catch (Exception e) {
+	            return new ResponseEntity<String>("{\"ERROR\":"+e.getMessage()+"\"}", headers, HttpStatus.INTERNAL_SERVER_ERROR);
+	        }
+	    }
+}
--- src/main/java/br/com/cashpack/model/RamoDeAtividade.java
+++ src/main/java/br/com/cashpack/model/RamoDeAtividade.java
@@ -1,16 +1,21 @@
 package br.com.cashpack.model;
+
+import javax.validation.constraints.NotNull;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.tostring.RooToString;
-import javax.validation.constraints.NotNull;
 
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
+@RooJson
 public class RamoDeAtividade {
 
-    /**
+	/**
      */
-    @NotNull
-    private String nome;
+	@NotNull
+	private String nome;
+
 }
