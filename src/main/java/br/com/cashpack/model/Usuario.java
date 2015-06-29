a2deb00ce91ef517e202ac921e0ec0991c3390a5
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2015-06-29 00:15:34 -0300

    Implementação da consulta de usuário por telefone

--- src/main/java/br/com/cashpack/model/Usuario.java
+++ src/main/java/br/com/cashpack/model/Usuario.java
@@ -1,12 +1,10 @@
 package br.com.cashpack.model;
-
 import javax.persistence.EntityManager;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.OneToOne;
 import javax.persistence.TypedQuery;
 import javax.validation.constraints.NotNull;
-
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
 import org.springframework.roo.addon.json.RooJson;
@@ -14,36 +12,32 @@ import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
-@RooJpaActiveRecord
 @RooJson
 @Inheritance(strategy = InheritanceType.JOINED)
+@RooJpaActiveRecord(finders = { "findUsuariosByCodigoPinAndTelefone" })
 public abstract class Usuario {
 
-	/**
+    /**
      */
-	@NotNull
-	@OneToOne
-	private Telefone telefone;
+    @NotNull
+    @OneToOne
+    private Telefone telefone;
 
-	/**
+    /**
      */
-	@NotNull
-	@OneToOne
-	private CodigoPIN codigoPin;
+    @NotNull
+    @OneToOne
+    private CodigoPIN codigoPin;
 
-	public static Usuario findUsuarioCashPackByCodPaisAndCodAreaAndNumero(
-			String codPais, String codArea, String numero) {
-
-		String sqlQuery = "SELECT u From Usuario u WHERE u.telefone.codPais =:codPais AND u.telefone.codArea =:codArea AND u.telefone.numero =:numero";
-
-		EntityManager manager = entityManager();
-		TypedQuery<Usuario> query = (TypedQuery<Usuario>) manager
-				.createQuery(sqlQuery);
-		query.setParameter("codPais", codPais);
-		query.setParameter("codArea", codArea);
-		query.setParameter("numero", numero);
-
-		return query.getSingleResult();
-	}
-
+    public static Usuario findUsuarioCashPackByCodPaisAndCodAreaAndNumero(String codPais, String codArea, String numero) {
+        String sqlQuery = "SELECT u From Usuario u WHERE u.telefone.codPais =:codPais AND u.telefone.codArea =:codArea AND u.telefone.numero =:numero";
+        
+        EntityManager manager = entityManager();
+        
+        TypedQuery<Usuario> query = (TypedQuery<Usuario>) manager.createQuery(sqlQuery);
+        query.setParameter("codPais", codPais);
+        query.setParameter("codArea", codArea);
+        query.setParameter("numero", numero);
+        return query.getSingleResult();
+    }
 }
