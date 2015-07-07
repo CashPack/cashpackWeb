82b8d76c60c4f119971b9e152937d3c197738dd7
Fernando M. de Oliveira <fernando.dsfw@gmail.com>
2015-07-06 22:38:43 -0300

    Implementação dos testes que exportam formato valido do JSON

--- src/test/java/br/com/cashpack/model/UsuarioCashPackDataOnDemand.java
+++ src/test/java/br/com/cashpack/model/UsuarioCashPackDataOnDemand.java
@@ -18,39 +18,39 @@ public class UsuarioCashPackDataOnDemand {
 
 		area = (String) numeroComArea.subSequence(0, 3);
 		area = area.replace("(", "").replace(")", "").replace(" ", "");
-		
+
 		numero = numeroComArea.substring(4).replace("(", "").replace(")", "")
 				.replace("-", "").replace(" ", "");
 
 		Assert.assertEquals("83", area);
 		Assert.assertEquals("988746463", numero);
 	}
-	
+
 	@Test
-	public void imprimeJsonNoFormatoDeRecebimentoDeUmUsuarioCashPack(){
+	public void imprimeJsonNoFormatoDeRecebimentoDeCadastroDeUmUsuarioCashPack() {
 		Telefone telefone = new Telefone();
 		telefone.setCodPais("55");
 		telefone.setNumero("(83) 98874 - 6463");
-		
+
 		UsuarioCashPack usuarioCashPack = new UsuarioCashPack();
 		usuarioCashPack.setTelefone(telefone);
-		
+
 		Gson gson = new Gson();
 		String json = gson.toJson(usuarioCashPack);
 		System.out.println(json);
 	}
-	
+
 	@Test
-	public void imprimeJsonNoFormatoDeRecebimentoDeUmaAgencia(){
+	public void imprimeJsonNoFormatoDeRecebimentoDeCadastroDeUmaAgencia() {
 		Telefone telefone = new Telefone();
 		telefone.setCodPais("55");
 		telefone.setNumero("(83) 98874 - 6463");
-		
+
 		RamoDeAtividade ramoDeAtividade = new RamoDeAtividade();
 		ramoDeAtividade.setId(1L);
 		ramoDeAtividade.setNome("Ramo de Atividade da Agência");
 		ramoDeAtividade.setVersion(0);
-		
+
 		// COM CPF
 		Agencia agencia = new Agencia();
 		agencia.setRazaoSocial("Razão Social da Agência");
@@ -60,15 +60,15 @@ public class UsuarioCashPackDataOnDemand {
 		agencia.setTipoDeDocumentoAgenciaEnum(TipoDeDocumentoDaAgenciaEnum.CPF);
 		agencia.setTelefone(telefone);
 		agencia.setRamoDeAtividade(ramoDeAtividade);
-		
+
 		Gson gson = new Gson();
 		String jsonCPF = gson.toJson(agencia);
 		System.out.println(jsonCPF);
-		
+
 		// COM CNPJ
 		agencia.setNumeroDocumento("52.836.475/0001-16");
 		agencia.setTipoDeDocumentoAgenciaEnum(TipoDeDocumentoDaAgenciaEnum.CNPJ);
-		
+
 		String jsonCNPJ = gson.toJson(agencia);
 		System.out.println(jsonCNPJ);
 	}
